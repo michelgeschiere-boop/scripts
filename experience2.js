b90188c47a00beb13b3bfcada213a8b8a1d36f85
michelgeschiere-boop <michelgeschiere@gmail.com>
2025-10-03 12:00:24 +0200

    Update experience2.js

--- experience2.js
+++ experience2.js
@@ -73,10 +73,10 @@ const splitHeadingTargets = document.querySelectorAll('[data-highlight-text]');
 if (!splitHeadingTargets.length) return;
 
 for (const heading of splitHeadingTargets) {
-const scrollStart  = heading.getAttribute('data-highlight-scroll-start') || 'top 60%';
-const scrollEnd    = heading.getAttribute('data-highlight-scroll-end')   || 'center 20%';
+const scrollStart  = heading.getAttribute('data-highlight-scroll-start') || 'top 90%';
+const scrollEnd    = heading.getAttribute('data-highlight-scroll-end')   || 'bottom 30%';
 const fadedValue   = parseFloat(heading.getAttribute('data-highlight-fade'))    || 0.4;
-const staggerValue = parseFloat(heading.getAttribute('data-highlight-stagger')) || 0.1;
+const staggerValue = parseFloat(heading.getAttribute('data-highlight-stagger')) || 0.2;
 
 new SplitText(heading, {
 type: 'words, chars',
